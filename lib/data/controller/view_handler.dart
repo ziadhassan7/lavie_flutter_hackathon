398acb1299a9f015bd4c7dc0214f482281b0ceca
ziadhassan7 <ziadhassan7@gmail.com>
2022-09-01 21:53:12 +0200

    check if screen is replaceable

--- lib/data/controller/view_handler.dart
+++ lib/data/controller/view_handler.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
-
 import '../../app_core/color_constants.dart';
 import '../../presentation/view/register/screen/register_screen.dart';
 
 class ViewHandler {
 
 
-  static void navigateTo(BuildContext context, Widget screen){
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => screen),
-    );
+  static void navigateTo(BuildContext context, Widget screen, {required bool isReplace}){
+    if (isReplace) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => screen),
+      );
+
+    } else {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => screen),
+      );
+    }
+
   }
 
   static void handleOutDatedAuth(context, e){
     print("Reeeeeess: why tho");
 
     if (e.response?.statusCode == 401){
-      navigateTo(context, RegisterScreen());
+      navigateTo(context, RegisterScreen(), isReplace: false);
       print("Reeeeeess: 401");
     }
   }
