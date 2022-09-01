fc488920b346c5918aab589f35ad84d2f2162705
ziadhassan7 <ziadhassan7@gmail.com>
2022-09-01 21:56:45 +0200

    get time by hour and minutes

--- lib/presentation/view/notifications/widget/notification_list_item.dart
+++ lib/presentation/view/notifications/widget/notification_list_item.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:la_vie_web/data/controller/time/time_handler.dart';
 import 'package:la_vie_web/presentation/view/common/text_poppins.dart';
 
 class NotificationListItem extends StatelessWidget {
-  static const int TODAY = 0;
-  static const int YEASTERDAY = 1;
-  static const int NORMAL_DATE = 2;
-
   String message;
   String time;
 
@@ -31,7 +28,7 @@ class NotificationListItem extends StatelessWidget {
           ),
 
           TextPoppins(
-            getFormattedTime(time),
+            TimeHandler.getFormattedTime(time),
             weight: FontWeight.w400,
             size: 12,
           ),
@@ -40,59 +37,7 @@ class NotificationListItem extends StatelessWidget {
     );
   }
 
-  String getFormattedTime (String time) {
-    DateTime dateTime = DateTime.parse(time);
-    int day = dateTime.day;
-    int month = dateTime.month;
-    int year = dateTime.year;
-
-    int hour = dateTime.hour;
-    int minute = dateTime.minute;
 
 
-    switch(timeStatus(day, month, year)) {
 
-      case TODAY:
-        return "Created today at ${twoDigitFormatter(hour)}:${twoDigitFormatter(minute)}";
-
-      case YEASTERDAY:
-        return "Created yesterday at ${twoDigitFormatter(hour)}:${twoDigitFormatter(minute)}";
-
-      case NORMAL_DATE:
-        return "Created at $day/$month/$year";
-
-      default:
-        return "";
-    }
-
-  }
-
-  int timeStatus (int day, int month, int year) {
-
-    if( day == DateTime.now().day
-        && month == DateTime.now().month
-        && year == DateTime.now().year) {
-
-      return TODAY;
-
-    } if ((day+1) == DateTime.now().day
-        && month == DateTime.now().month
-        && year == DateTime.now().year) {
-
-      return YEASTERDAY;
-
-    } else {
-      return NORMAL_DATE;
-    }
-
-  }
-
-  String twoDigitFormatter(int time) {
-
-    if(time < 10) {
-      return "0${time.toString()}";
-    } else {
-      return time.toString();
-    }
-  }
 }
